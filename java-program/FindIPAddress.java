3718e1b69c39b250949e6fc4e9f35cb52b27933d
wsngiri <prolink714@gmail.com>
2020-10-07 21:25:22 +0700

    change error handling

--- java-program/FindIPAddress.java
+++ java-program/FindIPAddress.java
@@ -1,19 +1,19 @@
+import java.io.IOException;
 import java.net.*;
 import java.util.*;
+
 public class FindIPAddress {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception {
 
         Scanner input = new Scanner(System.in);
         System.out.print("masukkan ip address: ");
         String host = input.next();
-        try {
-            InetAddress address = InetAddress.getByName(host);
-            System.out.println(
-                    "Nama Host : " + address.getHostName());
-        } catch (UnknownHostException uhEx) {
-            System.out.println(
-                    "Host not found:" + host);
-        }
+
+        InetAddress address = InetAddress.getByName(host);
+        System.out.println("Nama Host : " + address.getHostName());
+
+        System.out.println("Host not found:" + host);
+
     }
 }
